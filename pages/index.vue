8c9ca2c67f41c54aece998b1e78f0fd2c5bd8a7e
Enrico Deleo <hello@enricodeleo.com>
2020-11-23 17:37:21 +0100

    refactor: featured posts

--- pages/index.vue
+++ pages/index.vue
@@ -22,28 +22,36 @@
           <div v-for="post of featured" :key="post.id" class="card">
             <div class="row">
               <div class="col-md-5 wrapthumbnail">
-                <a href="post.html">
-                  <div class="thumbnail" style="background-image:url(assets/img/demopic/1.jpg);" />
+                <a :href="`/${post.slug}`" :title="post.title.rendered">
+                  <div class="thumbnail" :style="{'background-image': `url(${post.jetpack_featured_media_url})`}" />
                 </a>
               </div>
               <div class="col-md-7">
                 <div class="card-block">
-                  <h2 class="card-title">
-                    <a href="post.html">We're living some strange times</a>
+                  <h2 class="card-title pr-4 pt-4">
+                    <a :href="`/${post.slug}`" v-html="post.title.rendered" />
                   </h2>
-                  <h4 class="card-text">
-                    This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-                  </h4>
+                  <p class="card-text pr-4 text-muted">
+                    {{ post.meta._yoast_wpseo_metadesc[0] }}
+                  </p>
                   <div class="metafooter">
                     <div class="wrapfooter">
-                      <span class="meta-footer-thumb">
-                        <a href="author.html"><img class="author-thumb" src="https://www.gravatar.com/avatar/e56154546cf4be74e393c62d1ae9f9d4?s=250&amp;d=mm&amp;r=x" alt="Sal"></a>
-                      </span>
                       <span class="author-meta">
-                        <span class="post-name"><a href="author.html">Steve</a></span><br>
-                        <span class="post-date">22 July 2017</span><span class="dot" /><span class="post-read">6 min read</span>
+                        <span class="post-name">
+                          <a href="author.html">Sal</a>
+                        </span>
+                        <br>
+                        <span class="post-date">{{ post.dateLong }}</span>
+                        <span class="dot" />
+                        <span class="post-read">{{ Math.ceil(post.readingTime.minutes) }} minuti di lettura</span>
                       </span>
-                      <span class="post-read-more"><a href="post.html" title="Read Story"><svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25"><path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd" /></svg></a></span>
+                      <span class="post-read-more">
+                        <a :href="`/${post.slug}`" title="Leggi tutto">
+                          <svg class="svgIcon-use" width="25" height="25" viewbox="0 0 25 25">
+                            <path d="M19 6c0-1.1-.9-2-2-2H8c-1.1 0-2 .9-2 2v14.66h.012c.01.103.045.204.12.285a.5.5 0 0 0 .706.03L12.5 16.85l5.662 4.126a.508.508 0 0 0 .708-.03.5.5 0 0 0 .118-.285H19V6zm-6.838 9.97L7 19.636V6c0-.55.45-1 1-1h9c.55 0 1 .45 1 1v13.637l-5.162-3.668a.49.49 0 0 0-.676 0z" fill-rule="evenodd" />
+                          </svg>
+                        </a>
+                      </span>
                     </div>
                   </div>
                 </div>
@@ -78,7 +86,9 @@
                 <b-card-title v-html="post.title.rendered" />
               </a>
 
-              <b-card-text>{{ post.meta._yoast_wpseo_metadesc[0] }}</b-card-text>
+              <b-card-text class="text-muted">
+                {{ post.meta._yoast_wpseo_metadesc[0] }}
+              </b-card-text>
 
               <div class="metafooter">
                 <div class="wrapfooter">
